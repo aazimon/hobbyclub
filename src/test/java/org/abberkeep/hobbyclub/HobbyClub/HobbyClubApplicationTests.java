22ac5d6a2e4aa7ed67f0862e18e06c1f221daff9
Gary <gary@deken.net>
2025-03-25 10:53:54 -0500

    Sprint Initializer setup

---
+++ src/test/java/org/abberkeep/hobbyclub/HobbyClub/HobbyClubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abberkeep.hobbyclub.HobbyClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
